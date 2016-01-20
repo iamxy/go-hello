26343d22576bcf5c4392c496aa367178512b63ca
iamxy <kenneth.liu.xy@gmail.com>
2016-01-20 13:52:45 +0800

    first commit

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	for i := 0; i < 1000000; i++ {
+		if i%9 == 0 && i%4 == 1 && i%5 == 4 && i%6 == 3 && i%7 == 5 && i%8 == 1 {
+			fmt.Printf("%v\n", i)
+		}
+	}
+}
