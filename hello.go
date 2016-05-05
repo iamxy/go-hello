3d0bc06aa5171cc3749618534d4b5d80552ce1a0
iamxy <kenneth.liu.xy@gmail.com>
2016-05-05 15:25:00 +0800

    commit again.

--- hello.go
+++ hello.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func main() {
-	//fmt.Printf(stringutil.Reverse("!oG ,olleH"))
-	for i := 0; i < 1000000; i++ {
-		if i%9 == 0 && i%4 == 1 && i%5 == 4 && i%6 == 3 && i%7 == 5 && i%8 == 1 {
-			fmt.Printf("%v\n", i)
-		}
-	}
-	fmt.Println("finished !")
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(file)
+	fmt.Println(path)
 }
